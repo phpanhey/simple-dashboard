4753939ecc47f22ced557e4461518dd2e962ae33
philipp panhey <philipp.panhey@gmail.com>
2026-01-21 16:25:40 +0100

    added dynamic values from .env

--- simpledashboard.php
+++ simpledashboard.php
@@ -53,7 +53,7 @@
     }
 
     $mem       = calculateMemory();
-    $diskspace = calculateDiskSpace("/dev/vg1/volume_1");
+    $diskspace = calculateDiskSpace(getenv("DISK_SPACE_PATH"));
 
 ?>
 <div class="container">
@@ -90,7 +90,7 @@
  <path d="M5 7.8C5 6.11984 5 5.27976 5.32698 4.63803C5.6146 4.07354 6.07354 3.6146 6.63803 3.32698C7.27976 3 8.11984 3 9.8 3H14.2C15.8802 3 16.7202 3 17.362 3.32698C17.9265 3.6146 18.3854 4.07354 18.673 4.63803C19 5.27976 19 6.11984 19 7.8V21L12 17L5 21V7.8Z" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
  </svg>  fav</h3>
     <ul>
-      <li><a href="http://192.168.178.135:8000/dashboard">paperless ngx</a></li>
+    <li><a href=<?php echo getenv("PAPERLESS_URL")?>>paperless ngx</a></li>
     </ul>
 </article>
 </div>
